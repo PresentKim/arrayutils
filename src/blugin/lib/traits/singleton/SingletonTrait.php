6624d7ddfd09cad772c4d6f85f682407de5392b2
PresentKim <debe3721@gmail.com>
2020-09-14 21:54:56 +0900

    feat : Add virion 'SingletonTrait'

---
+++ src/blugin/lib/traits/singleton/SingletonTrait.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ *
+ *  ____  _             _         _____
+ * | __ )| |_   _  __ _(_)_ __   |_   _|__  __ _ _ __ ___
+ * |  _ \| | | | |/ _` | | '_ \    | |/ _ \/ _` | '_ ` _ \
+ * | |_) | | |_| | (_| | | | | |   | |  __/ (_| | | | | | |
+ * |____/|_|\__,_|\__, |_|_| |_|   |_|\___|\__,_|_| |_| |_|
+ *                |___/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ *
+ * @author  Blugin team
+ * @link    https://github.com/Blugin
+ * @license https://www.gnu.org/licenses/mit MIT License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ */
+
+declare(strict_types=1);
+
+namespace blugin\lib\traits\singleton;
+
+trait SingletonTrait{
+    /** @var self */
+    private static $instance = null;
+
+    public static function getInstance() : self{
+        if(self::$instance === null){
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+}
