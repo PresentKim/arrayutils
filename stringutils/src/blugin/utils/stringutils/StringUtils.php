fa107f266d879c9bf94ad0895bc90bd6d29810f9
PresentKim <debe3721@gmail.com>
2020-09-15 03:45:58 +0900

    feat : Add virion 'stringutils'

---
+++ stringutils/src/blugin/utils/stringutils/StringUtils.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ *
+ *  ____  _             _         _____
+ * | __ )| |_   _  __ _(_)_ __   |_   _|__  __ _ _ __ ___
+ * |  _ \| | | | |/ _` | | '_ \    | |/ _ \/ _` | '_ ` _ \
+ * | |_) | | |_| | (_| | | | | |   | |  __/ (_| | | | | | |
+ * |____/|_|\__,_|\__, |_|_| |_|   |_|\___|\__,_|_| |_| |_|
+ *                |___/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ *
+ * @author  Blugin team
+ * @link    https://github.com/Blugin
+ * @license https://www.gnu.org/licenses/mit MIT License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ */
+
+declare(strict_types=1);
+
+namespace blugin\lib\stringutils;
+
+class StringUtils{
+    private function __construct(){
+    }
+
+    public static function stripSpace(string $string) : string{
+        return preg_replace('/\s+/', '', $string);
+    }
+
+    public static function startsWith(string $haystack, string $needle) : bool{
+        return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== false;
+    }
+
+    public static function endsWith(string $haystack, string $needle) : bool{
+        return $needle === "" || substr($haystack, -strlen($needle)) === $needle;
+    }
+
+    public static function contains(string $haystack, string $needle) : bool{
+        return $needle === "" || strpos($haystack, $needle) !== false;
+    }
+}
