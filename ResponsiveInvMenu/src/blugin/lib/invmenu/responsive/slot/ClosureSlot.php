a562b4434a0867a756cbbf7e14e0c91a310171e0
PresentKim <debe3721@gmail.com>
2020-09-25 06:36:05 +0900

    feat : invmenu : Add ClosureSlot for simple generate slot

---
+++ ResponsiveInvMenu/src/blugin/lib/invmenu/responsive/slot/ClosureSlot.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ *
+ *  ____  _             _         _____
+ * | __ )| |_   _  __ _(_)_ __   |_   _|__  __ _ _ __ ___
+ * |  _ \| | | | |/ _` | | '_ \    | |/ _ \/ _` | '_ ` _ \
+ * | |_) | | |_| | (_| | | | | |   | |  __/ (_| | | | | | |
+ * |____/|_|\__,_|\__, |_|_| |_|   |_|\___|\__,_|_| |_| |_|
+ *                |___/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author  Blugin team
+ * @link    https://github.com/Blugin
+ * @license https://www.gnu.org/licenses/lgpl-3.0 LGPL-3.0 License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ */
+
+declare(strict_types=1);
+
+namespace blugin\lib\invmenu\responsive\slot;
+
+use pocketmine\item\Item;
+
+class ClosureSlot extends NormalItemSlot{
+    /** @var \Closure */
+    protected $closure;
+
+    public function __construct(Item $item, \Closure $closure){
+        parent::__construct($item);
+        $this->closure = $closure;
+    }
+
+    public function handleTransaction(SlotTransactionEvent $event) : bool{
+        return ($this->closure)($event)();
+    }
+}
